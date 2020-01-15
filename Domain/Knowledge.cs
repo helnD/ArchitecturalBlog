5f5b016dd529c1a7ae09d4a2050acc950daeb646
Vladimir <vladimirrudt@yandex.ru>
2020-01-15 13:11:54 +0700

    The entity "knowledge" was added

---
+++ Domain/Knowledge.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Knowledge
+    {
+        public Knowledge(string title, string description, string type,
+            IEnumerable<Knowledge> associatedKnowledge)
+        {
+            Title = title;
+            Description = description;
+            Type = type;
+            AssociatedKnowledge = associatedKnowledge;
+        }
+
+        public string Title { get; }
+        public string Description { get; }
+        public string Type { get; }
+        public IEnumerable<Knowledge> AssociatedKnowledge { get; }
+        
+    }
+}
